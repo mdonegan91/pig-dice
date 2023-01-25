94a1f24a5c99d4d25f12903fd5725a7af1d54ab7
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-25 13:09:22 -0800

    WIP

---
+++ README.md
@@ -0,0 +1,16 @@
+Describe: rollDice()
+
+Test: "It should randomly generate a number from 1 to 6."
+Code: rollDice()
+Expected Output: 1, 2, ... 6
+
+Describe: isOne()
+
+Test: "It should check if the roll is equal to 1"
+Code: rollDice()
+Expected Output: 1 === true
+
+Describe: scoreTotal()
+
+Test: "It should total the numbers rolled"
+Code: 
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,49 @@
+let turnScore = 0;
+let roll = 0;
+let totalScore1 = 0;
+let totalScore2 = 0;
+
 function rollDice() {
-    return Math.floor(Math.random() * 6 + 1);
-};
+    const roll = Math.floor(Math.random() * 6) + 1;
+    return roll;
+}
+
+function isOne(roll) {
+    return roll === 1;
+}
+
+//player 1
+function scoreTotal(turnScore, totalScore1) {
+    roll = rollDice();
+    if (roll === 1) { // if(roll !== 1 ) {
+        turnScore = 0;
+        console.log("player1 score: " + roll)
+        return totalScore1;
+    } else {
+        turnScore += roll
+        totalScore1 += turnScore;
+        return totalScore1;
+    }
+}
+-----------------------------------------------------------------------------------
+function PigDice(player1, player2) {
+    this.player1 = {name: player1, score: 0 }
+    this.player2 = {name: player2, score: 0 }
+    this.currentRoll = 0;
+    this.turnScore = 0;
+    this.currentPlayer = 1;
+}
+
+PigDice.prototype.rollDice = function() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+PigDice.prototype.playGame = function() {
+    //while(score < 100) {
+    this.currentRoll = rollDice();
+    if(this.currentRoll === 1) {
+        this.player1.turnScore = 0;
+    } else {
+        this.player1.turnScore += this.currentRoll;
+    }
+}
